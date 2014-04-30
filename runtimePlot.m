3b7f2b1d22235967a8ceff5d1f190a78a1e22335
Spencer Lee <lesp@seas.upenn.edu>
2014-04-30 10:50:38 -0400

    Updated runtime plot

--- runtimePlot.m
+++ runtimePlot.m
@@ -1,9 +1,16 @@
 hold on;
-nLines = [200 500 1000 5000 10000];
-runTime = [20 72 147 480 2017];
-plot(nLines,runTime,'b-');
-plot(nLines,runTime,'ro');
+nLines = [200 500 1000 2000 4231 5123 6221 7112 9231 10000];
+runTime = [20 72 147 250 500 670 1011 1340 1822 2017];
+plot(nLines,runTime,'b-','linewidth',2);
+h=plot(nLines,runTime,'r.');
+set(h,'MarkerSize',25)
+
 xlabel('Number of lines');
 ylabel('Time (ms)');
 title('Runtime vs. Number of Lines');
+figureHandle = gcf;
+%# make all text in the figure to size 14 and bold
+a = get(gca,'XTickLabel');
+%set(gca,'XTickLabel',a,'FontName','Times','fontsize',14,'fontWeight','bold')
+%set(findall(figureHandle,'type','text'),'fontSize',16,'fontWeight','bold')
 hold off;
